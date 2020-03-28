7bd31ee75747e1834f24567a81a1ddb4f2683933
dbzabhilash <32972846+dbzabhilash@users.noreply.github.com>
2020-03-28 15:36:10 -0500

    making Node class

---
+++ src/Node.java
@@ -0,0 +1,38 @@
+public class Node {
+    private Interval interv;
+    private int imax;
+    private int priority;
+    public Node(Interval i){
+        interv = i;
+        //TODO: generate a priority for the node
+
+    }
+    public Node getParent(){
+        //TODO:
+        return null;
+    }
+
+    public Node getLeft(){
+        //TODO:
+        return null;
+    }
+
+    public Node getRight(){
+        //TODO:
+        return null;
+    }
+
+    public Interval getInterv() {
+        return interv;
+    }
+
+    public int getIMax(){
+        return imax;
+    }
+    public int getPriority(){
+        return priority;
+    }
+
+
+
+}
