007c8f9b862889ca2575ae9f14b7c293cef52d09
dbzabhilash <32972846+dbzabhilash@users.noreply.github.com>
2020-03-28 15:02:47 -0500

    Finished making Interval class

--- src/Interval.java
+++ src/Interval.java
@@ -1,2 +1,16 @@
 public class Interval {
+    private int low;
+    private int high;
+    public Interval (int low, int high){
+        this.low = low;
+        this.high = high;
+    }
+
+    public int getHigh() {
+        return high;
+    }
+
+    public int getLow() {
+        return low;
+    }
 }
