be4b791013dc39cdf1ae706d59366c5c0e72748d
dbzabhilash <32972846+dbzabhilash@users.noreply.github.com>
2020-03-29 00:52:06 -0500

    Finished Node class

--- src/IntervalTreap.java
+++ src/IntervalTreap.java
@@ -1,10 +1,24 @@
 public class IntervalTreap {
-    private Node root = null;
+    private Node root;  // root of this treap.
     private int size;   // number of nodes in the treap.
     private int height; // height of the treap.
 
     public IntervalTreap(){
+        root = null;
+        size = 0;
+        height = 0;
+    }
 
+    public Node getRoot() {
+        return root;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getHeight() {
+        return height;
     }
 
     public void intervalInsert(Node z){
--- src/Node.java
+++ src/Node.java
@@ -1,14 +1,17 @@
+import java.util.Random;
+
 public class Node {
     private Node left = null;
     private Node right = null;
     private Node parent = null;
     private Interval interv;
-    private int imax;
+    private int imax = -1;      // set to -1 by default, TODO: consider changing to interv.getHigh()
     private int priority;
     public Node(Interval i){
         interv = i;
         //TODO: generate a priority for the node
-
+        Random random = new Random();
+        priority = random.nextInt(Integer.MAX_VALUE);
     }
     public Node getParent(){
         return parent;
@@ -43,6 +46,9 @@ public class Node {
     }
     public void setRight(Node right){
         this.right = right;
+    }
+    public int setIMax(int newImax){
+        this.imax = newImax;
     }
 
 }
