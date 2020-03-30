c02b814a86ac941e5449cc9651417b245ba3d9c3
dbzabhilash <32972846+dbzabhilash@users.noreply.github.com>
2020-03-30 02:55:13 -0500

    written but untested: intervalInsert()

--- src/IntervalTreap.java
+++ src/IntervalTreap.java
@@ -6,7 +6,7 @@ public class IntervalTreap {
     public IntervalTreap(){
         root = null;
         size = 0;
-        height = 0;
+        height = -1;    // empty treaps have -1 height
     }
 
     public Node getRoot() {
@@ -23,15 +23,53 @@ public class IntervalTreap {
 
     public void intervalInsert(Node z){
         size++;     // update size to reflect new number of nodes in treap
+        height++;   // update height to reflect new number of nodes in treap
+        if(root == null) root = z;
+        else{
+            Node temp = new Node(root.getInterv());
+            Node lastVisit = null;
+            int lastLeft = -1;
+            int key = z.getInterv().getLow();
+            while(temp!=null){
+                lastVisit = temp;
+                if(key < temp.getInterv().getLow()){
+                    temp = temp.getLeft();
+                    lastLeft = 1;
+                }
+                else{
+                    temp = temp.getRight();
+                    lastLeft = 0;
+                }
+            }
+            z.setParent(lastVisit);
 
+            //  setting lastVisit's children
+            if(lastLeft==1){
+                lastVisit.setLeft(z);
+            }
+            else if(lastLeft==0){
+                lastVisit.setRight(z);
+            }
+            else  System.out.println("Error: lastLeft not initialized, lastLeft ="+ lastLeft);
+            // lastLeft = -1 if not initialized otherwise
+
+            while(z!=root && z.getPriority() < z.getParent().getPriority()){
+                if(z.getParent().getLeft().equals(z)){
+                    rightRotate(z);
+                }
+                else{
+                    leftRotate(z);
+                }
+            }
+        }
     }
 
     public void intervalDelete(Node z){
-
+        //TODO: decrease the size by 1 upon successfully deleting node
     }
 
     public Node intervalSearch(Interval i){
-        Node temp = root;
+        Node temp = new Node(root.getInterv());
         while(temp!=null && !overlap(temp.getInterv(),i)){
             if(temp.getLeft().getIMax()>=i.getLow() && temp.getLeft()!=null){
                 temp = temp.getLeft();
@@ -42,9 +80,56 @@ public class IntervalTreap {
         }
         return temp;
     }
-
+    //helper methods
     public boolean overlap(Interval y,Interval z){
         if(y.getLow()<=z.getHigh() && z.getLow()<=y.getHigh())   return true;
         else return false;
+    }
+
+    public void rightRotate(Node z){
+        z.getParent().setLeft(z.getRight());
+        z.setRight(z.getParent());
+        Node paa = z.getParent();
+
+        if(paa.equals(root)){
+            root = z;
+        }
+        else{   // making relations with grandparent
+            if(paa.getParent().getLeft().equals(paa)){
+                // My paa's a left child
+                paa.getParent().setLeft(z);
+            }
+            else {
+                // My paa's a right child
+                paa.getParent().setRight(z);
+            }
+        }
+        z.setParent(paa.getParent());      //TODO: paa.Parent might be null
+        //  I am my original paa's parent
+        paa.setParent(z);
+
+    }
+
+    public void leftRotate(Node z){
+        z.getParent().setRight(z.getLeft());
+        z.setLeft(z.getParent());
+        Node paa = z.getParent();
+
+        if(paa.equals(root)){
+            root = z;
+        }
+        else{   // making relations with grandparent
+            if(paa.getParent().getLeft().equals(paa)){
+                // My paa's a left child
+                paa.getParent().setLeft(z);
+            }
+            else {
+                // My paa's a right child
+                paa.getParent().setRight(z);
+            }
+        }
+        z.setParent(paa.getParent());      //TODO: paa.Parent might be null
+        //  I am my original paa's parent
+        paa.setParent(z);
     }
 }
--- src/mainRunner.java
+++ src/mainRunner.java
@@ -10,18 +10,33 @@ public class mainRunner {
 
         Random rand = new Random();
         int randHigh, randLow;
-        for(int n=0; n<25; n++){
-            randLow = rand.nextInt(100);
-            randHigh = rand.nextInt(100) + randLow;
-            i = new Interval(randLow,randHigh);
-            nd = new Node(i);
-            ndPrio = nd.getPriority();
-            ndImax = nd.getIMax();
-            System.out.println("Node nd"+(n+1)+" stores interval " +
-                    "("+randLow+","+randHigh+") has priority: "+ ndPrio+" and Imax: "+ ndImax);
+//        for(int n=0; n<25; n++){
+//            randLow = rand.nextInt(100);
+//            randHigh = rand.nextInt(100) + randLow;
+//            i = new Interval(randLow,randHigh);
+//            nd = new Node(i);
+//            ndPrio = nd.getPriority();
+//            ndImax = nd.getIMax();
+//            System.out.println("Node nd"+(n+1)+" stores interval " +
+//                    "("+randLow+","+randHigh+") has priority: "+ ndPrio+" and Imax: "+ ndImax);
+//
+//        }
 
-        }
+        //  Deep object copy test
+        Node newND = new Node(nd.getInterv());
+        newND.setIMax(34);
+        System.out.println("Node nd imax = "+nd.getIMax());
+        System.out.println("Node newND imax = "+newND.getIMax());
+
+        //  Test Nullable
+        newND.setParent(null);
+        nd.setParent(newND.getParent());
+        System.out.println("Node newND parent = "+newND.getParent());
+        System.out.println("Node nd parent = "+nd.getParent());
+
+        //  Test treap insertion
         IntervalTreap T;
+        T.
 
     }
 
