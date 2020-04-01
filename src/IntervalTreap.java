0c8c02e389d45a8e7b8802286f1857aa183636ec
abir99 <abir99@iastate.edu>
2020-04-01 02:02:23 -0500

    test push

--- src/IntervalTreap.java
+++ src/IntervalTreap.java
@@ -3,7 +3,7 @@ public class IntervalTreap {
     private int size;   // number of nodes in the treap.
     private int height; // height of the treap.
 
-    public IntervalTreap(){
+    public IntervalTreap() {
         root = null;
         size = 0;
         height = -1;    // empty treaps have -1 height
@@ -21,87 +21,94 @@ public class IntervalTreap {
         return height;
     }
 
-    public void intervalInsert(Node z){
+    public void intervalInsert(Node z) {
         size++;     // update size to reflect new number of nodes in treap
 //        height++;   // update height to reflect new number of nodes in treap
-        if(root == null) root = z;
-        else{
+        if (root == null) root = z;
+        else {
             Node temp = root; //dont make this new lmao
             Node lastVisit = null;
             int lastLeft = -1;
             int key = z.getInterv().getLow();
-            while(temp!=null){
-                if(key < temp.getInterv().getLow()){
+
+            while (temp != null) {
+                if (key < temp.getInterv().getLow()) {
 
                     lastLeft = 1;
-                    if(temp.getLeft()==null) break;
-                    temp = temp.getLeft();
-                }
-                else{
+                    if (temp.getLeft() == null) {
+                        temp.setIMax(Math.max(temp.getIMax(), z.getIMax()));
+                        break;
+                    } else {
+                        temp.setIMax(Math.max(temp.getIMax(), z.getIMax()));
+                        temp = temp.getLeft();
+                    }
+
+                } else {
                     lastLeft = 0;
-                    if(temp.getRight()==null) break;
-                    temp = temp.getRight();
+                    if (temp.getRight() == null) {
+                        temp.setIMax(Math.max(temp.getIMax(), z.getIMax()));
+                        break;
+                    } else {
+                        temp.setIMax(Math.max(temp.getIMax(), z.getIMax()));
+                        temp = temp.getRight();
+                    }
+
                 }
             }
             z.setParent(temp);
 
             //  setting lastVisit's children
-            if(lastLeft==1){
+            if (lastLeft == 1) {
                 temp.setLeft(z);
-            }
-            else if(lastLeft==0){
+            } else if (lastLeft == 0) {
                 temp.setRight(z);
-            }
-            else  System.out.println("Error: lastLeft not initialized, lastLeft ="+ lastLeft);
+            } else System.out.println("Error: lastLeft not initialized, lastLeft =" + lastLeft);
             // lastLeft = -1 if not initialized otherwise
 
-            while(z!=root && z.getPriority() < z.getParent().getPriority()){
-                if(z.getParent().getLeft()==z){
+            while (z != root && z.getPriority() < z.getParent().getPriority()) {
+                if (z.getParent().getLeft() == z) {
                     rightRotate(z);
-                }
-                else{
+                } else {
                     leftRotate(z);
                 }
             }
         }
     }
 
-    public void intervalDelete(Node z){
+    public void intervalDelete(Node z) {
         //TODO: decrease the size by 1 upon successfully deleting node
     }
 
-    public Node intervalSearch(Interval i){
+    public Node intervalSearch(Interval i) {
         Node temp = root;
-        while(temp!=null && !overlap(temp.getInterv(),i)){
-            if(temp.getLeft().getIMax()>=i.getLow() && temp.getLeft()!=null){
+        while (temp != null && !overlap(temp.getInterv(), i)) {
+            if (temp.getLeft().getIMax() >= i.getLow() && temp.getLeft() != null) {
                 temp = temp.getLeft();
-            }
-            else{
+            } else {
                 temp = temp.getRight();
             }
         }
         return temp;
     }
+
     //helper methods
-    public boolean overlap(Interval y,Interval z){
-        if(y.getLow()<=z.getHigh() && z.getLow()<=y.getHigh())   return true;
+    public boolean overlap(Interval y, Interval z) {
+        if (y.getLow() <= z.getHigh() && z.getLow() <= y.getHigh()) return true;
         else return false;
     }
 
-    public void rightRotate(Node z){
+    public void rightRotate(Node z) {
         z.getParent().setLeft(z.getRight());
         z.setRight(z.getParent());
         Node paa = z.getParent();
 
-        if(paa == root){
+        if (paa == root) {
             root = z;
-        }
-        else{   // making relations with grandparent
-            if(paa.getParent().getLeft()==paa){ //check if left child
+        } else {   // making relations with grandparent
+            if (paa.getParent().getLeft() == paa) { //check if left child
                 // My paa's a left child
                 paa.getParent().setLeft(z);
-            }
-            else {
+            } else {
                 // My paa's a right child
                 paa.getParent().setRight(z);
             }
@@ -112,20 +119,18 @@ public class IntervalTreap {
 
     }
 
-    public void leftRotate(Node z){
+    public void leftRotate(Node z) {
         z.getParent().setRight(z.getLeft());
         z.setLeft(z.getParent());
         Node paa = z.getParent();
 
-        if(paa == root){
+        if (paa == root) {
             root = z;
-        }
-        else{   // making relations with grandparent
-            if(paa.getParent().getLeft()==paa){
+        } else {   // making relations with grandparent
+            if (paa.getParent().getLeft() == paa) {
                 // My paa's a left child
                 paa.getParent().setLeft(z);
-            }
-            else {
+            } else {
                 // My paa's a right child
                 paa.getParent().setRight(z);
             }
